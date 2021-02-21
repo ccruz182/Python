ad8918b275422e133d80c09083fa83c28b900cf0
César Cruz A <cesar.cruz182@gmail.com>
2021-02-21 11:05:02 -0600

    web scrapping 1

---
+++ web-scrapping/article_scraper/article_scraper/pipelines.py
@@ -0,0 +1,25 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+from scrapy.exceptions import DropItem
+from datetime import datetime
+
+
+class CheckItemPipeline:
+    def process_item(self, article, spider):
+        if not article['last_updated'] or not article['url'] or not article['title']:
+            raise DropItem('Missing something!')
+        return article
+
+class CleanDatePipeline:
+    def process_item(self, article, spider):
+        article['last_updated'] = article['last_updated'].replace('This page was last edited on', '').strip()
+        article['last_updated'] = datetime.strptime(article['last_updated'], '%d %B %Y, at %H:%M')
+
+        return article
+
